75ab450ac8ca06f5e895353ff61d463c2af0c3e7
ALAE MAGHCHICH <alaemaghchich2004@gmail.com>
2025-10-15 13:43:03 +0100

    js

--- prototype 2/main.js
+++ prototype 2/main.js
@@ -1,11 +1,8 @@
 const checkboxes = document.querySelectorAll('.list input[type="checkbox"]');
-const counter = document.getElementById('counter');
-
-function alae() {
-  const completed = document.querySelectorAll('.list input[type="checkbox"]:checked').length;
+let counter = document.getElementById("counter");
+function alae(){
+  let completed = document.querySelectorAll('.list input[type="checkbox"]:checked').length;
   const total = checkboxes.length;
-  counter.textContent = completed + " / " + total + " completed tasks";
-}
-checkboxes.forEach(checkbox => {
-  checkbox.addEventListener('change',alae );
-});
+  counter.textContent = completed + " / " + total + " completed tasks";}
+for (let box of checkboxes){
+  box.onchange = alae;}
